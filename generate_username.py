501b4e215aaffde442b61cfd42237b476af6768a
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-09-21 15:23:10 +0530

    Add files via upload

---
+++ generate_username.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+users = set()
+
+length = 8
+characters = string.ascii_letters + string.digits
+
+while len(users) < 10:
+    # username = ''.join(random.choice(characters) for i in range(length))
+    username_chars = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in
+                      range(length)]
+    username = ''.join(username_chars)
+
+    if username not in users:
+        users.add(username)
+        print("Generated Username:", username)
