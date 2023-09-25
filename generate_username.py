bff157f79afd6de6e37f1f0519309417c62000eb
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-09-25 12:15:24 +0530

    Update generate_username.py

--- generate_username.py
+++ generate_username.py
@@ -5,13 +5,17 @@ users = set()
 
 length = 8
 characters = string.ascii_letters + string.digits
+def generate_unique_username():
+    while len(users) < 10:
+        # username = ''.join(random.choice(characters) for i in range(length))
+        username_chars = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in
+                          range(length)]
+        username = ''.join(username_chars)
 
-while len(users) < 10:
-    # username = ''.join(random.choice(characters) for i in range(length))
-    username_chars = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in
-                      range(length)]
-    username = ''.join(username_chars)
+        if username not in users:
+            users.add(username)
+            yield username
 
-    if username not in users:
-        users.add(username)
-        print("Generated Username:", username)
+obj=generate_unique_username()
+for i in obj:
+    print("unique username :",i)
