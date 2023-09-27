64d21daf086a9cada0858ab5c034cfcca33d35d5
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-09-27 14:25:41 +0530

    Update content_management_system.py

--- content_management_system.py
+++ content_management_system.py
@@ -1,15 +1,16 @@
 def create_blog_post():
     title = input("Enter the title of the blog post: ")
     content = input("Enter the content of the blog post: ")
-    filename = f"{title}.txt"
+    filename = r"C:\Users\JagadabhiBalaji-3303\PycharmProjects\pythonProject\projects\Rajeswari tasks\{title}.txt"
     with open(filename, "w") as file:
         file.write(content)
     print(f"Blog post '{title}' created successfully!")
 
 def read_blog_post():
     title = input("Enter the title of the blog post you want to read: ")
+    filename = r"C:\Users\JagadabhiBalaji-3303\PycharmProjects\pythonProject\projects\Rajeswari tasks\{title}.txt"
     try:
-        with open(f"{title}.txt", "r") as file:
+        with open(filename,"r") as file:
             content = file.read()
             print(content)
     except FileNotFoundError:
@@ -17,11 +18,11 @@ def read_blog_post():
 
 def edit_blog_post():
     title = input("Enter the title of the blog post you want to edit: ")
-    filename = f"{title}.txt"
+    filename = r"C:\Users\JagadabhiBalaji-3303\PycharmProjects\pythonProject\projects\Rajeswari tasks\{title}.txt"
     try:
         with open(filename, "r") as file:
             content = file.read()
-
+            
         print(f"Current content of '{title}':")
         print(content)
         old_word = input("Enter the word you want to replace: ")
